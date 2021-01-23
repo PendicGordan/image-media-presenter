aa1404215de071dada060e96fe3698479f4cc02a
Gordan Pendic <gordan.pendic5@gmail.com>
2021-01-23 17:52:36 +0100

    Changing icons for tab view.

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -129,13 +129,9 @@
             });
 
             let btnAdd = document.getElementById('btnAdd');
-            // Installation must be done by a user gesture! Here, the button click
             btnAdd.addEventListener('click', () => {
-                // hide our user interface that shows our A2HS button
                 btnAdd.style.display = 'none';
-                // Show the prompt
                 deferredPrompt.prompt();
-                // Wait for the user to respond to the prompt
                 deferredPrompt.userChoice
                     .then((choiceResult) => {
                         if (choiceResult.outcome === 'accepted') {
