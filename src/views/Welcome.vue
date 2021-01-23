c3053b09f4c40434d3bd1822315a5f5d2a1d5711
Gordan Pendic <gordan.pendic5@gmail.com>
2021-01-23 16:56:56 +0100

    Testing PWA button

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -38,6 +38,9 @@
                             New presentation
                             <v-icon>mdi-new-box</v-icon>
                         </v-btn>
+                        <v-btn id="btnAdd">
+                            <label class="btn" style>Add to desktop</label>
+                        </v-btn>
                     </template>
                     <v-card>
                         <v-card-text style="height: 67px;">
@@ -114,6 +117,36 @@
                 this.setPresentationName(this.presentationName);
                 this.$router.push({ path: 'editor', query: { new: true } });
             }
+        },
+        mounted() {
+            let deferredPrompt;
+
+            window.addEventListener('beforeinstallprompt', function(e) {
+                // Prevent Chrome 67 and earlier from automatically showing the prompt
+                e.preventDefault();
+                // Stash the event so it can be triggered later.
+                deferredPrompt = e;
+            });
+
+            let btnAdd = document.getElementById('btnAdd');
+            // Installation must be done by a user gesture! Here, the button click
+            btnAdd.addEventListener('click', () => {
+                // hide our user interface that shows our A2HS button
+                btnAdd.style.display = 'none';
+                // Show the prompt
+                deferredPrompt.prompt();
+                // Wait for the user to respond to the prompt
+                deferredPrompt.userChoice
+                    .then((choiceResult) => {
+                        if (choiceResult.outcome === 'accepted') {
+                            console.log('User accepted the A2HS prompt');
+                        } else {
+                            console.log('User dismissed the A2HS prompt');
+                        }
+                        deferredPrompt = null;
+                    });
+            });
+
         }
     }
 </script>
